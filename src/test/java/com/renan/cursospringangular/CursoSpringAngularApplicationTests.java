adef351b282c30aea570ae0d0ba6c0cd2c822249
Renan-FS <renanfernandes12@hotmail.com>
2020-08-26 22:01:42 -0300

    Projeto criado

---
+++ src/test/java/com/renan/cursospringangular/CursoSpringAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renan.cursospringangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
