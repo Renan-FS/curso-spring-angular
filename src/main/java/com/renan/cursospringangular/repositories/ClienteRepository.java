3c11cb750d104c60b0073415f67c3d5fef19ad7c
Renan-FS <renanfernandes12@hotmail.com>
2020-09-04 16:58:58 -0300

    Validação customizada

--- src/main/java/com/renan/cursospringangular/repositories/ClienteRepository.java
+++ src/main/java/com/renan/cursospringangular/repositories/ClienteRepository.java
@@ -2,10 +2,14 @@ package com.renan.cursospringangular.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.renan.cursospringangular.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+	
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
 
 }
