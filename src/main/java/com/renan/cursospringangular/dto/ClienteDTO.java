21f3984f45cc34c662df3470ee8a05c2156a822b
Renan-FS <renanfernandes12@hotmail.com>
2020-09-09 17:17:17 -0300

    Busca de Produtos por nome e categorias

--- src/main/java/com/renan/cursospringangular/dto/ClienteDTO.java
+++ src/main/java/com/renan/cursospringangular/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.renan.cursospringangular.domain.Cliente;
+import com.renan.cursospringangular.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 
--- src/main/java/com/renan/cursospringangular/repositories/ProdutoRepository.java
+++ src/main/java/com/renan/cursospringangular/repositories/ProdutoRepository.java
@@ -1,11 +1,22 @@
 package com.renan.cursospringangular.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.renan.cursospringangular.domain.Categoria;
 import com.renan.cursospringangular.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 
+	@Transactional(readOnly = true)
+	@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(@Param("nome") String nome,@Param("categoria") List<Categoria> categorias, Pageable pageRequest);
 }
