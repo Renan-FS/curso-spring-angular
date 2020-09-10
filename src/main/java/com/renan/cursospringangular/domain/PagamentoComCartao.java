3f071fd38c04b711f236e57230e9efba9ac507c6
Renan-FS <renanfernandes12@hotmail.com>
2020-09-10 10:40:24 -0300

    Subtotal e total do Pedido

--- src/main/java/com/renan/cursospringangular/domain/PagamentoComCartao.java
+++ src/main/java/com/renan/cursospringangular/domain/PagamentoComCartao.java
@@ -2,9 +2,11 @@ package com.renan.cursospringangular.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.renan.cursospringangular.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento{
 	private static final long serialVersionUID = 1L;
 
