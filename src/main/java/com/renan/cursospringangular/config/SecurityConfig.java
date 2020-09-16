4a02bfafd72190b10d75959af3ce346d91795193
Renan-FS <renanfernandes12@hotmail.com>
2020-09-16 17:26:02 -0300

    Autorizando endpoints para perfis especificos

--- src/main/java/com/renan/cursospringangular/config/SecurityConfig.java
+++ src/main/java/com/renan/cursospringangular/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -20,6 +21,7 @@ import com.renan.cursospringangular.security.JWTAuthenticationFilter;
 import com.renan.cursospringangular.security.JWTAuthorizationFilter;
 import com.renan.cursospringangular.security.JWTUtil;
 
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 	@Autowired
@@ -40,6 +42,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			"/categorias/**",
 			"/clientes/**"
 	};
+	
+	private static final String[] PUBLIC_MATCHERS_POST = {
+			"/clientes/**"
+	};
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -50,6 +56,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 		http.cors().and().csrf().disable();
 		http.authorizeRequests()
+			.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
 			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
