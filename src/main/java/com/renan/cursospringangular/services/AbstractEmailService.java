e16cecddb2ba0c7a381cea5763692367cdb24ce2
Renan-FS <renanfernandes12@hotmail.com>
2020-09-17 14:08:55 -0300

    Esqueci a senha

--- src/main/java/com/renan/cursospringangular/services/AbstractEmailService.java
+++ src/main/java/com/renan/cursospringangular/services/AbstractEmailService.java
@@ -13,6 +13,7 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import com.renan.cursospringangular.domain.Cliente;
 import com.renan.cursospringangular.domain.Pedido;
 
 public abstract class AbstractEmailService implements EmailService{
@@ -29,6 +30,23 @@ public abstract class AbstractEmailService implements EmailService{
 	public void sendOrderConfirmationEmail(Pedido obj) {
 		SimpleMailMessage sm = prepareSimpleMailMessageFromPedido(obj);
 		sendEmail(sm);
+	}
+	
+
+	@Override
+	public void sendNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = prepareNewPasswordEmail(cliente, newPass);
+		sendEmail(sm);
+	}
+
+	protected SimpleMailMessage prepareNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = new SimpleMailMessage();
+		sm.setTo(cliente.getEmail());
+		sm.setFrom(sender);
+		sm.setSubject("Solicitação de nova senha");
+		sm.setSentDate(new Date(System.currentTimeMillis()));
+		sm.setText("Nova senha: " + newPass);
+		return sm;
 	}
 	
 	protected SimpleMailMessage prepareSimpleMailMessageFromPedido(Pedido obj) {
